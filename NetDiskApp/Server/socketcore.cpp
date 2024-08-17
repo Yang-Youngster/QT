75931c98c4f47ec69d9a1b24d70dc1fc2b13c936
肥肠教主 <yangyoungster@163.com>
2024-08-17 23:20:26 +0800

    协议功能

--- NetDiskApp/Server/socketcore.cpp
+++ NetDiskApp/Server/socketcore.cpp
@@ -2,24 +2,33 @@
 
 SocketCore::SocketCore()
 {
+
     //通过readyRead函数激发信号 调用SLOT中的函数
     connect(this,SIGNAL(readyRead()),this,SLOT(receiveMsg()));
     connect(this,SIGNAL(disconnected()),this,SLOT(clientoffline()));
 
-    //是否启用文件上传
+    //功能是否启用文件上传
 }
-
-
+//获取本对象的用户名
+QString SocketCore::getClientName()
+{
+  return client_Name;
+}
 //处理收到的信息
 void receiveMsg()
 {
-    
-    
+
 
 }
-
-
 void offline(SocketCore* socket)
 {
-
+    //状态下线
+    QString ClientName=socket->getClientName();
+    if(ClientName==NULL)
+    {
+        throw "字符串不能为空" ;
+    }
+    //提示下线
+    OpenDB::getInstance().handleOffline(ClientName);
+    qDebug()<<"账户："<< ClientName <<"成功下线！";
 }
